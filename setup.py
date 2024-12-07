5d5aed31f774b91d0f93f88e8a69e83e35a75680
vile319 <vile319@gmail.com>
2024-12-07 18:44:09 -0500

    huh

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sql_upsert",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "sqlalchemy>=1.4.0",
--- sql_upsert/__init__.py
+++ sql_upsert/__init__.py
@@ -1,4 +1,4 @@
 from .upsert import to_sql_upsert
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["to_sql_upsert"] 
